6a1a27977f8ea0576244323ef7e0520cf30d0f43
Hargun Singh Khera <hargunsinghkhera8@gmail.com>
2022-11-03 15:41:05 +0530

    first

---
+++ coupon.js
@@ -0,0 +1,59 @@
+function firstCoupon() {
+    alert("Congratulations! Your voucher of Amazon has been applied successfully.");
+    document.getElementById('btn-first').innerHTML = "Applied Successfully";
+    document.getElementById('btn-second').style.display = "none";
+    document.getElementById('btn-third').style.display = "none";
+    document.getElementById('btn-fourth').style.display = "none";
+    document.getElementById('btn-fifth').style.display = "none";
+    document.getElementById('btn-sixth').style.display = "none";
+}
+
+function secondCoupon() {
+    alert("Congratulations! Your voucher of McDonalds has been applied successfully.");
+    document.getElementById('btn-second').innerHTML = "Applied Successfully";
+    document.getElementById('btn-first').style.display = "none";
+    document.getElementById('btn-third').style.display = "none";
+    document.getElementById('btn-fourth').style.display = "none";
+    document.getElementById('btn-fifth').style.display = "none";
+    document.getElementById('btn-sixth').style.display = "none";
+}
+
+function thirdCoupon() {
+    alert("Congratulations! Your voucher of KFC has been applied successfully.");
+    document.getElementById('btn-third').innerHTML = "Applied Successfully";
+    document.getElementById('btn-first').style.display = "none";
+    document.getElementById('btn-second').style.display = "none";
+    document.getElementById('btn-fourth').style.display = "none";
+    document.getElementById('btn-fifth').style.display = "none";
+    document.getElementById('btn-sixth').style.display = "none";
+}
+
+function fourthCoupon() {
+    alert("Congratulations! Your voucher of Nike has been applied successfully.");
+    document.getElementById('btn-fourth').innerHTML = "Applied Successfully";
+    document.getElementById('btn-first').style.display = "none";
+    document.getElementById('btn-second').style.display = "none";
+    document.getElementById('btn-third').style.display = "none";
+    document.getElementById('btn-fifth').style.display = "none";
+    document.getElementById('btn-sixth').style.display = "none";
+}
+
+function fifthCoupon() {
+    alert("Congratulations! Your voucher of Zomato has been applied successfully.");
+    document.getElementById('btn-fifth').innerHTML = "Applied Successfully";
+    document.getElementById('btn-first').style.display = "none";
+    document.getElementById('btn-second').style.display = "none";
+    document.getElementById('btn-third').style.display = "none";
+    document.getElementById('btn-fourth').style.display = "none";
+    document.getElementById('btn-sixth').style.display = "none";
+}
+
+function sixthCoupon() {
+    alert("Congratulations! Your voucher of Nike has been applied successfully.");
+    document.getElementById('btn-sixth').innerHTML = "Applied Successfully";
+    document.getElementById('btn-first').style.display = "none";
+    document.getElementById('btn-second').style.display = "none";
+    document.getElementById('btn-third').style.display = "none";
+    document.getElementById('btn-fourth').style.display = "none";
+    document.getElementById('btn-fifth').style.display = "none";
+}
